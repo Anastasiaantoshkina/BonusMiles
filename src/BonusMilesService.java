51ee18cb462735a3997db237b6b7728c4713ed06
Anastasia <vesheneba2020@gmail.comgit config --global user.email vesheneba2020@gmail.com>
2024-06-10 15:12:14 +0300

    three commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,12 @@
+public class BonusMilesService {
+
+    public int calculate(int price) {
+        int mile;
+        int bonus = 20;
+
+         mile = price / bonus;
+         return mile;
+
+
+    }
+}
